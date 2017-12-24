9e3c7c172349277d9723f812c4abc9875b5cf6df
Gurpreet Singh <montycarlo017@gmail.com>
2017-12-24 22:32:17 +0000

    Add files via upload

---
+++ print function and strings.py
@@ -0,0 +1,10 @@
+print ("this is to print what-soever is present in this quotes.")
+
+print ('This is what the escape character do i.e. escaping the single quote "blah blah\'s" ')
+
+print ("Concatinate 2 strings using + sign:" + " Like this")
+
+print ("Concatinate 2 strings using , symbol(it automatically inserts space):" , "Like this")
+
+print ("Concatinate 2 strings using , symbol(it automatically inserts space):" , 999)
+#but cant be done with +, or we can typecast it using str()
