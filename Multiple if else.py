26425d51bee4b9f945176ed05d272b5caaee405c
Gurpreet Singh <montycarlo017@gmail.com>
2017-12-28 23:18:12 +0000

    Add files via upload

---
+++ Multiple if else.py
@@ -0,0 +1,12 @@
+a=5
+b=4
+c=2
+if b<a:
+    print("a is the largest")
+elif c>a:
+    print("c is largest")
+elif 3>100:
+    print("Numerics compared")
+# this is true but it doesen't run because initially a condition is true and then it leaves the conditional statement.
+else:
+    print("I am in else part")
---
+++ if statement.py
@@ -0,0 +1,8 @@
+a=5
+b=4
+c=2
+d=3
+if b<a > c > d:
+    print("a is the largest")
+else:
+    print("Test")
