34b8f60ebc335b71f341e605359f554abaf8b475
Gurpreet Singh <montycarlo017@gmail.com>
2017-12-29 01:37:43 +0000

    Add files via upload

---
+++ While loop.py
@@ -0,0 +1,9 @@
+hola=1
+while hola < 5:
+    print("This is boogyman:", hola)
+    hola+=1 #incrementing the variable hola 1 in every iteration
+
+while True:
+    print("This is going to be infinite loop:")
+    # This is going to be the infinite loop.
+    # So to interrupt press "ctrl+c" or Stop in Pycharm or Jupyter
