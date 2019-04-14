6947fd0ed1bd541b160ba9c4a39ebf95a695b1c8
Gurpreet Singh <montycarlo017@gmail.com>
2019-04-14 14:55:58 +0100

    Update leap_year_function.py

--- leap_year_function.py
+++ leap_year_function.py
@@ -1,11 +1,15 @@
 def is_leap(year):
     leap = False
     
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                leap = True
+    # if year % 4 == 0:
+    #     if year % 100 == 0:
+    #         if year % 400 == 0:
+    #             leap = True
+    # 1992
     
+
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        leap = True
     return leap
 
 year = int(input())
