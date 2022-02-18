71131f85ae879d216125b6b5f482a0b9b2c1ed2f
Gurpreet Singh <montycarlo017@gmail.com>
2022-02-18 20:44:17 +0000

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+dognames = ['alpha','bravo','charlie','delta']
+print(dognames)
+
+dognames.insert(0,'echo')
+print(dognames)
+
+print(dognames[2])
+del(dognames[3])
+print(dognames)
+
+print(len(dognames))
+
+
+dognames[2] = 'Tiger'
+print(dognames)
